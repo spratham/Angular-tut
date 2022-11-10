25fbd18fc756936a54a39f86b095101111fd7333
Pratham_singh <pratham@teamvariance.com>
2022-11-10 16:22:42 +0530

    Child-to-parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Basic-todo-app';
-  list: any[] = [];
-  getValue(item: string) {
-    this.list.push({ id: this.list.length, name: item }); //pushing id and item(input data) to the array
-    console.warn(this.list);
-  }
-  removeId(id: number) {
-    this.list = this.list.filter((item) => item.id!==id); //filtering the undeleted items from list
-    console.warn(id);
-  }
+  title = 'parent-child-data';
+  data=10;
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+// inputs:['item'], //way 1
+
 })
 export class LoginComponent implements OnInit {
-
+// item:any;       //way 1
   constructor() { }
-
+ @ Input() item:any;  //value could be random -input decorator  //way 2
   ngOnInit(): void {
   }
 
