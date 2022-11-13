7d4f2523345f5b6c8a033af661ab318363208879
Pratham_singh <pratham@teamvariance.com>
2022-11-13 19:30:10 +0530

    2-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'parent-child-data';
-  data=10;
-
+  title = 'Two-way binding';
+  name:any;
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,13 +4,10 @@ import { Component, OnInit ,Input} from '@angular/core';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-// inputs:['item'], //way 1
 
 })
 export class LoginComponent implements OnInit {
-// item:any;       //way 1
-  constructor() { }
- @ Input() item:any;  //value could be random -input decorator  //way 2
+
   ngOnInit(): void {
   }
 
